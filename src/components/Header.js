fd9d0dafc3cd22aafff9d1afde1ed96a218ee188
simicodes <simeonoffiong@gmail.com>
2022-08-02 17:27:44 +0100

    Header components updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,27 @@
 import React from "react";
 
+//import components
+import Logo from "../assets/img/logo.svg";
+import Nav from "./Nav";
+
+//import icons
+import { HiMenu } from "react-icons/hi";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="py-6">
+      <div className="container mx-auto">
+        {/* logo */}
+        <a href="#!">
+          <img src={Logo} alt="" />
+        </a>
+        {/* nav */}
+        <Nav />
+        {/* nav mobile btn "react-modal-video": "^1.2.9",*/}
+        <HiMenu className="lg:hidden text-white cursor-pointer" />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
