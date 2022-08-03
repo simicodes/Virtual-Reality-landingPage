dab16d6ec036cb2a3f9715d7accf1755b33a4193
simicodes <simeonoffiong@gmail.com>
2022-08-03 09:45:52 +0100

    Header commponents Navbar

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import NavMobile from "./components/NavMobile";
 const App = () => {
   const [navMobile, setNavMobile] = useState(false);
   return (
-    <div>
+    <div className="relative overflow-hidden before:w-[600px] before:h-[200px] before:bg-circle before:bg-no-repeat before:absolute before:-top-16 before:left-[600px] before:hidden before:lg:flex">
       <Header setNavMobile={setNavMobile} />
       <Banner />
       {/* nav mobile */}
@@ -17,8 +17,9 @@ const App = () => {
           navMobile ? "right-0" : "-right-full"
         } fixed top-0 bottom-0 w-48 transition-all`}
       >
-        <NavMobile setNavMobile={setNavMobile}/>
+        <NavMobile setNavMobile={setNavMobile} />
       </div>
+      
     </div>
   );
 };
